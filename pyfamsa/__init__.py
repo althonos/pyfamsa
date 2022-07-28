c45efec14ae498ae1e62f71f7fa126f1a9efaf8d
Martin Larralde <martin.larralde@embl.de>
2022-07-28 16:31:51 +0200

    Import public classes from the `_famsa` extension module to the `pyfamsa` namespace

---
+++ pyfamsa/__init__.py
@@ -0,0 +1,21 @@
+# noqa: D104
+from ._version import __version__  # isort: skip
+
+from . import _famsa
+from ._famsa import (
+    Alignment,
+    Aligner,
+    Sequence,
+    GappedSequence,
+)
+
+__doc__ = _famsa.__doc__
+__all__ = [
+    "Alignment",
+    "Aligner",
+    "Sequence",
+    "GappedSequence"
+]
+
+__author__ = "Martin Larralde <martin.larralde@embl.de>"
+__license__ = "GPLv3"
