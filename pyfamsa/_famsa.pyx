c648b1cd3ce411000f3b2a18cdca80b7bd74a607
Martin Larralde <martin.larralde@embl.de>
2023-08-20 16:26:46 +0200

    Disable use of memory-monotonic to make code thread safe

--- pyfamsa/_famsa.pyx
+++ pyfamsa/_famsa.pyx
@@ -110,9 +110,9 @@ cdef class Sequence:
     # --- Magic methods ------------------------------------------------------
 
     def __init__(self, bytes id, bytes sequence):
-        self._cseq = move(CSequence(id, sequence, 0, MMA))
+        self._cseq = move(CSequence(id, sequence, 0, NULL))
         self._shape[0] = self._cseq.length
-        assert self._cseq.mma is not NULL
+        # assert self._cseq.mma is not NULL
 
     def __copy__(self):
         return self.copy()
